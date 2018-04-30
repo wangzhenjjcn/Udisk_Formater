2b31b38dfcd1c43d1808a1e3541d905f59bc219d
WangZhen <wangzhenjjcn@gmail.com>
2018-04-30 10:56:34 +0800

    cls

--- U-Disk-Format_tools.bat
+++ U-Disk-Format_tools.bat
@@ -1,9 +1,10 @@
 @echo off
+MODE con: COLS=160 LINES=50
 set filepath=%~dp0
 cd %filepath%
 >nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"  
 if '%errorlevel%' NEQ '0' (  
-echo RequestAdminAccess...  
+echo request Admin Access...  
 goto UACPrompt  
 ) else ( goto gotAdmin )   
 :UACPrompt   
@@ -11,6 +12,7 @@ echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
 echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"  
 "%temp%\getadmin.vbs"  
 exit /B  
-:gotAdmin  
+:gotAdmin
+cls
 python run.py
 pause;
